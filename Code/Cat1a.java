ad8457a9356d9bfc85776a5f70649548b50cf102
Jason Nguyen <2055141@student.curtin.edu.au>
2022-05-24 16:06:50 +0800

    Updated Cat1a.java 	- Altered Menu to have line breaks to look nicer 	- Added extra line of text to ask for user input 	- Created loop for asking user to confirm their string 	- Created switch case for asking user to confirm their string

--- Code/Cat1a.java
+++ Code/Cat1a.java
@@ -9,7 +9,7 @@ public class Cat1a
         Scanner sc = new Scanner(System.in);
         boolean runningOne = true;
 
-        System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = =");
+        System.out.println("\n= = = = = = = = = = = = = = = = = = = = = = = = = = =");
         System.out.println("Welcome to the Uppercase Lowercase Converter Program");
         System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = =");
         System.out.println("");
@@ -57,15 +57,53 @@ public class Cat1a
         System.out.println(" > (R)ead from a file?");
         System.out.println(" > (M)anually enter data?");
         System.out.println(" > (E)xit program");
+        System.out.println("\n");
+        System.out.print("Please enter your selection: ");
     }
 
     public static void runManualMode()
     {
+        Scanner sc = new Scanner(System.in);
+        boolean runningStringConfirm = true;
+        String confirmInput;
+        String stringInput;
+
+        while(runningStringConfirm == true)
+        {
+            System.out.print("\nPlease enter the string you wish to convert: ");
+
+            stringInput = sc.nextLine();
+            
+            System.out.println("\nYou have entered: " + stringInput);
+            System.out.println("Would you like to continue? ");
+            System.out.println(" > (Y)es ");
+            System.out.println(" > (N)o ");
+            
+            confirmInput = sc.nextLine().toLowerCase();
+
+            switch(confirmInput)
+            {
+                case "y":
+                    runningStringConfirm = false;
+                    break;
+
+                case "n":
+                    runningStringConfirm = true;
+                    break;
+                
+                default:
+                    System.out.println("That input was invalid");
+            }
+
+        }
+
+
+
 
     }
 
     public static void runReadMode()
     {
-        
+
     }
 }
