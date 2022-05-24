c3a99643bf85b0545a7f635058175292ed544b15
Jason Nguyen <2055141@student.curtin.edu.au>
2022-05-24 15:24:39 +0800

    Updated Main.java 	- Cases now call the respective class files and run their main functions 	- Created while loop to run Main.java menu in loop 	- Created runningMain variable for above purpose 	- Created case and option to exit menu loop, thus exitting program 	- Moved case into while loop 	- Moved Menu printing into while loop

--- Code/Main.java
+++ Code/Main.java
@@ -5,41 +5,50 @@ public class Main
 {
 	public static void main(String args[])
 	{
-		System.out.println("Select which function to run");
-
-		System.out.println("(1) Cat.1a > Convert given string to upper case or lower case");
-		System.out.println("(2) Cat.1b > Identify whether numeric values are in a given string");
-		System.out.println("(3) Cat.1c > Identify whether a given string is a valid number");
-		System.out.println("(4) Cat.1d > Remove numeric values in a given string then conver to upper or lower case");
-		System.out.println("(5) Cat.2c > Convert time");
-		
 		Scanner sc = new Scanner(System.in);
-
+		boolean runningMain = true;
+		
 		String input = sc.next();
-
-		switch(input)
+		
+		while(runningMain == true)
 		{
-			case "1":
-				//
-				break;
+			System.out.println("Select which function to run");
+	
+			System.out.println("(0) > Exit");
+			System.out.println("(1) Cat.1a > Convert given string to upper case or lower case");
+			System.out.println("(2) Cat.1b > Identify whether numeric values are in a given string");
+			System.out.println("(3) Cat.1c > Identify whether a given string is a valid number");
+			System.out.println("(4) Cat.1d > Remove numeric values in a given string then conver to upper or lower case");
+			System.out.println("(5) Cat.2c > Convert time");
 
-			case "2":
-				//
-				break;
-
-			case "3":
-				//
-				break;
-
-			case "4":
-				//
-				break;
-
-			case "5":
-				//
-				break;
-			default:
-				System.out.println("Choice was invalid");
+			switch(input)
+			{
+				case "0":
+					runningMain = false;
+					break;
+				case "1":
+					Cat1a.main(args);
+					break;
+	
+				case "2":
+					Cat1b.main(args);
+					break;
+	
+				case "3":
+					Cat1c.main(args);
+					break;
+	
+				case "4":
+					Cat1d.main(args);
+					break;
+	
+				case "5":
+					Cat2c.main(args);
+					break;
+				default:
+					System.out.println("Choice was invalid");
+			}
+			
 		}
 
 		sc.close();
