5d96c52813dea4c26e2e8f179e5b41e10aa36377
Jason Nguyen <2055141@student.curtin.edu.au>
2022-05-24 15:00:27 +0800

    Updated Cat1a.java 	- Added initial query of Manual or Read mode 	- Setup printing menu 	- Setup cases 	- Setup empty runManualMode() function 	- Setup empty runReadMode() funciton

--- Code/Cat1a.java
+++ Code/Cat1a.java
@@ -1,7 +1,71 @@
 import java.util.*;
 import java.io.*;
+import java.text.*;
 
 public class Cat1a
 {
-    
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        boolean runningOne = true;
+
+        System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = =");
+        System.out.println("Welcome to the Uppercase Lowercase Converter Program");
+        System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = =");
+        System.out.println("");
+
+        while(runningOne == true)
+        {
+            printInputType();
+            
+            String input = sc.next().toLowerCase();
+
+            switch(input)
+            {
+                case "e":
+                    runningOne = false;
+                    break;
+                case "exit":
+                    runningOne = false;
+                    break;
+
+                case "m":
+                    runManualMode();
+                    break;
+                case "manual":
+                    runManualMode();
+                    break;
+
+                case "r":
+                    runReadMode();
+                    break;
+                case "read":
+                    runReadMode();
+                    break;
+
+                default:
+                    System.out.println("The input was invalid, please try again.");
+            }
+        }
+        
+        
+    }
+
+    public static void printInputType()
+    {
+        System.out.println("Would you like to: ");
+        System.out.println(" > (R)ead from a file?");
+        System.out.println(" > (M)anually enter data?");
+        System.out.println(" > (E)xit program");
+    }
+
+    public static void runManualMode()
+    {
+
+    }
+
+    public static void runReadMode()
+    {
+        
+    }
 }
