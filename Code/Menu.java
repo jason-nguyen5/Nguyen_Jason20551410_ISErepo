bcdf46a958d3462407775e2435dd0f83bcae2414
Jason Nguyen <2055141@student.curtin.edu.au>
2022-05-26 16:08:55 +0800

    Updated Menu.java 	- Created, Wrote, and Finished inputTypeMenu 	- Created, Wrote, and Finished runManualInput 	- Created runAutomaticInput

--- Code/Menu.java
+++ Code/Menu.java
@@ -6,8 +6,11 @@ public class Menu
 	public static void main(String args[])
 	{
 		Scanner sc = new Scanner(System.in);
+		List<char[]> listOfStrings = new ArrayList<char[]>();
 		boolean runningMain = true;
 		
+		listOfStrings = inputTypeMenu();
+
 		while(runningMain == true)
 		{
 			System.out.println("\nSelect which function to run:\n");
@@ -54,5 +57,106 @@ public class Menu
 		}
 
 	}
+
+	public static List<char[]> inputTypeMenu()
+	{
+		Scanner sc = new Scanner(System.in);
+		List<char[]> returnList = new ArrayList<char[]>();
+		boolean runningInputTypeMenu = true;
+
+
+
+		while(runningInputTypeMenu == true)
+		{
+			System.out.println("\nHow would you like to enter you data?");
+			System.out.println(" > (R)ead or (A)utomatically from a file?");
+			System.out.println(" > (M)anually enter the string?");
+			System.out.println(" > (E)xit");
+
+			System.out.print("\nPlease enter your choice: ");
+
+			String input = sc.next().toLowerCase();
+			
+			switch(input)
+			{
+				case "e":
+					runningInputTypeMenu = false;
+					break;
+				case "m":
+					returnList = runManualInput();
+					runningInputTypeMenu = false;
+					break;
+				case "r":
+					returnList = runAutomaticInput();
+					runningInputTypeMenu = false;
+					break;
+				case "a":
+					returnList = runAutomaticInput();
+					runningInputTypeMenu = false;
+					break;
+				default:
+					System.out.println("Choice was invalid");
+			}
+		}
+
+		return returnList;
+	}
+
+	public static List<char[]> runManualInput()
+	{
+		Scanner sc = new Scanner(System.in);
+        boolean runningStringConfirm = true;
+        String stringInput = "";
+		List<char[]> returnList = new ArrayList<char[]>();
+		
+        while(runningStringConfirm == true)
+        {
+			System.out.print("\nPlease enter the string you wish to convert: ");
+			
+            stringInput = sc.nextLine();
+            
+            System.out.println("\nYou have entered: " + stringInput);
+            System.out.println("\nWould you like to continue? ");
+            System.out.println(" > (Y)es ");
+            System.out.println(" > (N)o ");
+            System.out.print("\nPlease make your selection: ");
+            
+            String confirmInput = sc.nextLine().toLowerCase();
+			
+            switch(confirmInput)
+            {
+				case "y":
+				runningStringConfirm = false;
+				break;
+				
+                case "n":
+				runningStringConfirm = true;
+				break;
+                
+                default:
+				System.out.println("That input was invalid");
+            }
+		}
+		
+		char[] charArray = new char[stringInput.length()];
+
+		for(int i = 0; i < stringInput.length(); i++)
+		{
+			charArray[i] = stringInput.charAt(i);
+
+		}
+
+		returnList.add(charArray);
+
+		return returnList;
+	}
+
+	public static List<char[]> runAutomaticInput()
+	{
+		//
+	}
+
+
+
 
 }
