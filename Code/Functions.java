e61c229bc55e41c8248059634413dd61e0e0ea12
Jason Nguyen <2055141@student.curtin.edu.au>
2022-05-26 15:17:18 +0800

    Wrote and Finished code for charArrayToAsciiArray Added, Wrote, and FInished code for asciiArrayToString

--- Code/Functions.java
+++ Code/Functions.java
@@ -2,7 +2,27 @@ public class Functions
 {
 	public static int[] charArrayToAsciiArray(char[] inputArray)
 	{
-		
+		int[] returnArray = new int[inputArray.length - 1];
+
+		for(int i = 0; i < inputArray.length; i++)
+		{
+			returnArray[i] = (int)inputArray[i];
+		}
+	
+		return returnArray;
 	} 
 
+	public static String asciiArrayToString(int[] inputArray)
+	{
+		String returnString = "";
+
+		for(int i = 0; i < inputArray.length; i++)
+		{
+			returnString = returnString + (char)inputArray[i];
+		}
+
+		return returnString;
+	}
+
+	
 }
